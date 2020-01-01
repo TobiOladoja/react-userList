e25bdaae59903e2a8ad8560eb6923af4274ee55a
TobiOladoja <tbfullstack@gmail.com>
2020-01-01 23:24:38 +0000

    Added three components into small web app, and added description and picture from randomuser in person component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
 import './App.css';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => (
+  // App Component
+  // In this app function we're going to render the PersonList
+  <PersonList />
+);
+
+const PersonList = () => {
+  // PersonList Component
+  // Not passing props here, only to person component.
+  // Gonna render the Person  component
+  return <Person> </Person>;
+};
+
+const Person = () => {
+  // Person Component
+  // Most of our work will be done here in the Person
+  const url = 'https://randomuser.me/api/portraits/thumb/men/75.jpg';
+
+  return (
+    <div className='person'>
+      <img src={url} alt='' />
+      <div>
+        <h4>Name</h4>
+        <h4>Job</h4>
+      </div>
+    </div>
+  );
+};
 
 export default App;
