5e34117afcbfae5199bc133e99bf29e36382bf6f
TobiOladoja <tbfullstack@gmail.com>
2020-01-01 23:35:33 +0000

    Duplicated person component three times

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import './App.css';
 
+// App Component, the app is here just to be rendered. In this app function we're going to render the PersonList
 const App = () => (
-  // App Component
-  // In this app function we're going to render the PersonList
+  // Blank
   <PersonList />
 );
 
+// PersonList Component Not passing props here, only to person component. Gonna render the Person  component
 const PersonList = () => {
-  // PersonList Component
-  // Not passing props here, only to person component.
-  // Gonna render the Person  component
-  return <Person> </Person>;
+  // Created an array where we will place people, and within the array there will be an object
+  const people = [
+    {
+      img: 22,
+      name: 'john',
+      job: 'developer'
+    },
+    {
+      img: 34,
+      name: 'sam',
+      job: 'footballer'
+    },
+    {
+      img: 56,
+      name: 'tony',
+      job: 'customer service assistant'
+    }
+  ];
+
+  return (
+    <section>
+      {/* Essentially just duplicated person component three times*/}
+      <Person />
+      <Person />
+      <Person />
+    </section>
+  );
 };
 
 const Person = () => {
